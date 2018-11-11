23c55df904a5ad2c9d650be79bc32508a385bc49
Ksenia Pelipenko <astattee@gmail.com>
2018-11-12 00:04:48 +0300

    [added] new models & migrations

---
+++ app/models/mark.rb
@@ -0,0 +1,8 @@
+class Mark < ApplicationRecord
+  belongs_to :user
+  belongs_to :post
+
+  validates :mark, presence: true
+  validates_numericality_of :mark, only_integer: true, :greater_than => 0, :less_than => 6
+
+end
